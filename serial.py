7f613ac2f685efee33a24d029085b9cab5a0c0c0
calebthewood <calebwood.cs@gmail.com>
2022-03-22 16:23:34 -0700

    adds serial generator class

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Initializes instance of SerialGenerator class with counting capabilities."""
+        self.counter = start-1
+        self.start = start
+
+    def __repr__(self):
+        return f"<SerialGenerator start = {self.start} counter = {self.counter}>"
+
+    def generate(self):
+        """Increments the instance's counter by 1, and returns count value."""
+        self.counter+=1
+        return self.counter
+
+    def reset(self):
+        """Resets the instance's counter to starting value. Returns None"""
+        self.counter = self.start-1
+
+serial = SerialGenerator(start=100)
