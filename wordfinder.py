5605ca5b12d345854dba57d8107d56218aafc0be
Eric Sohng <ericsohng@gmail.com>
2022-03-22 17:35:13 -0700

    create subclass specialwordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,43 @@
+import random
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, filepath):
+        """Instantiates wordfinder and creates wordlist from file"""
+        self.filepath = filepath
+        self.wordlist = self.get_wordlist()
+
+
+    def __repr__(self):
+        return f"<WordFinder wordlist={self.wordlist} filepath={self.filepath}>"
+    
+    def get_wordlist(self):
+        """Takes file of words and returns words in a list"""
+        wordfile = open(self.filepath)
+        wordlist = [line.replace('\n', '') for line in wordfile]
+        return wordlist
+    
+    def random(self):
+        """Returns a random word from a list of words"""
+        return random.choice(self.wordlist)
+       
+wf = WordFinder('words.txt')
+
+
+
+class SpecialWordFinder(WordFinder):
+    """DOCSTRING"""
+
+    def __init__(self, filepath):
+        super().__init__(filepath)
+        self.wordlist = self.refine_list()
+
+    def refine_list(self):
+        # ['# Veggies', '', 'kale', 'parsnips', '', '# Fruits', etc]
+        new_word_list = [word for word in self.wordlist if word]
+        new_word_list = [word for word in new_word_list if not word.startswith('#')]
+        return new_word_list
+
+
+swf = SpecialWordFinder('fancywords.txt')
